1248ec66bda9e00050ec1cb5bb9df579f8dc7cc6
dragos <slujerudragos@gmail.com>
2019-02-25 01:32:47 +0200

    implemented word counter for all files in a root directory

--- src/word_counter/core.clj
+++ src/word_counter/core.clj
@@ -1,12 +1,20 @@
 (ns word-counter.core
+  (:require [clojure.java.io :as io])
+  (:require [clojure.pprint :as pp])
   (:gen-class))
 
-(defn foo
-  [a b]
-  (if a 
-    (println a)
-    (println b)))
+(defn get-frequencies
+  [arg]
+  (map #(frequencies (re-seq #"(?m)\w+" (slurp (.getAbsolutePath %)))) arg)
+)
+
+(defn merge-map
+  [arg1 arg2]
+  (merge-with + arg1 arg2))
 
 (defn -main
   [& args]
-  (foo nil "bbbb"))
+  (def f (clojure.java.io/file "resources"))
+  (def fs (file-seq f))
+  (def only-files (filter #(.isFile %) fs))
+  (println (reduce merge-map (get-frequencies only-files))))
